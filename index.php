ebc511746770638b9755fccb5a7bbcb43c3f2d23
gopibabus <s.gopibabu@gmail.com>
2020-02-09 16:39:31 -0600

    added standard response model

--- index.php
+++ index.php
@@ -1,3 +1,27 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+    <title>PHP API</title>
+    <link
+        rel="stylesheet"
+        href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
+    />
+    <script src="https://kit.fontawesome.com/078c5ccadb.js" crossorigin="anonymous"></script>
+</head>
+<body>
+<div class="container">
+    <h1 class="text-center display-3">PHP API</h1>
+    <h3 class="text-center display-4">Following are list of APIs available</h3>
+    <a href="#" class="btn btn-lg btn-primary form-control mt-5">GET TASKS</a>
 
-phpinfo();
+</div>
+
+
+<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+</body>
+</html>
